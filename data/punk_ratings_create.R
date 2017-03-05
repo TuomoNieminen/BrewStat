dee46d970ac8be035efecbcf0d18969a55964b84
Tuomo Nieminen <tuomo.a.nieminen@helsinki.fi>
2017-03-06 01:45:36 +0200

    create punk ratings data

--- data/punk_ratings_create.R
+++ data/punk_ratings_create.R
@@ -17,20 +17,19 @@ html <- read_html(url)
 
 # exclude nodes describing the beer type. 
 # This is done because a single column includes beer names and other info. (we want the beer names separately)
-# What this actually achieves is not clear, perhaps gives the label of the element instead?
+# What this actually achieves is not clear, perhaps gives the label of the element instead...
 # This turns out to work anyway because the label info is separated from beernames with multiple spaces and includes even more information
 tmp <- xml_find_all(html, xpath_exclude) %>% xml_remove
 
-# create a data frame fromt the html table
-beer_ratings <- html_nodes(html, xpath = xpath) %>% 
-  html_table(header = T, fill = T)
-
-beer_ratings = beer_ratings[[1]]
+# create a data frame from the html table
+beer_ratings <- html_nodes(html, xpath = xpath) %>% html_table(header = T, fill = T)
+beer_ratings <- beer_ratings[[1]]
 
 # remove two ghost columns
 beer_ratings <- beer_ratings[, -c(4,8)]
 
-# problem is the the first column ('Name') includes the beer names along with other information.
+# Problem is the the first column ('Name') includes the beer names along with other information.
+# Use string manipulation to fix
 # -------------------------------------
 
 # keep only beers brewed for BrewDog
@@ -39,7 +38,7 @@ brewdog_ratings <- dplyr::filter(beer_ratings, grepl("BrewDog", beer_ratings$Nam
 # exclude collaboration beers (they have a '/' in the 'Name' column)
 brewdog_ratings <- dplyr::filter(brewdog_ratings, !grepl("/", brewdog_ratings$Name))
 
-# remove the 'BrewDog' from Name to get the beer name and type
+# remove the 'BrewDog' from the 'Name' column to get only the beer name and type
 nametype <- gsub("BrewDog ", "", brewdog_ratings$Name)
 
 # remove unneeded text inside parenthesis such as '(retired)'
@@ -48,8 +47,10 @@ nametype <- gsub("*\\(.*?\\) *", "", nametype)
 # split text to beer name and beer type according to two or more whitespace
 name_type <- reshape2::colsplit(nametype, "\\s{2,}", names = c("beer_name", "beer_type"))
 
-# add beername and type to data, remove the 'Name' column and rename the columns
+# add beername and type to the rating data frame
 ratings <- cbind(brewdog_ratings, name_type)
+
+# remove the 'Name' column and rename the columns
 ratings <- ratings[-1]
 names(ratings) <- paste0("ratebeer_", names(ratings))
 names(ratings)[5] <- "ratebeer_rank"
